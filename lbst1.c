a6a8e22c22c70d463b4c6b713ae42c29b69766d4
Hasitha <hasithadananjaya894@gmail.com>
2023-11-13 16:43:51 +0530

    First question of labsheet 01

---
+++ lbst1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+    printf("Name : Hasitha Dananjaya\n");
+    printf("Index Number : 1333539\n");
+    printf("Address : 12/3 Pitipana South Presannapura Homagama\n");
+    printf("Date of Birth : 13.11.2023");
+}
